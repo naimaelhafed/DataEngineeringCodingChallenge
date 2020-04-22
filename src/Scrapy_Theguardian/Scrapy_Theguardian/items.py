dd82f00fbc7faf80243675ab4177b48bb70f321b
naimaelhafed <hafed.naima@gmail.com>
2020-04-22 16:34:44 +0000

    Version Scrapy

--- src/Scrapy_Theguardian/Scrapy_Theguardian/items.py
+++ src/Scrapy_Theguardian/Scrapy_Theguardian/items.py
@@ -8,7 +8,10 @@
 import scrapy
 
 
-class ScrapyTheguardianItem(scrapy.Item):
-    # define the fields for your item here like:
-    # name = scrapy.Field()
+class ArticleItem(scrapy.Item):
+    author = scrapy.Field()
+    headline = scrapy.Field()
+    text = scrapy.Field()
+    url = scrapy.Field()
+    
     pass
--- src/Scrapy_Theguardian/Scrapy_Theguardian/pipelines.py
+++ src/Scrapy_Theguardian/Scrapy_Theguardian/pipelines.py
@@ -5,7 +5,39 @@
 # Don't forget to add your pipeline to the ITEM_PIPELINES setting
 # See: https://docs.scrapy.org/en/latest/topics/item-pipeline.html
 
+import pymongo
 
-class ScrapyTheguardianPipeline(object):
+class CrawlertheguardianPipeline(object):
+
+    collection_name = 'articles'
+
+    def __init__(self, mongo_uri, mongo_db):
+        self.mongo_uri = mongo_uri
+        self.mongo_db = mongo_db
+
+    @classmethod
+    def from_crawler(cls, crawler):
+        return cls(
+            mongo_uri=crawler.settings.get('MONGO_URI'),
+            mongo_db=crawler.settings.get('MONGO_DATABASE', 'items')
+        )
+
+    def open_spider(self, spider):
+        self.client = pymongo.MongoClient(self.mongo_uri)
+        self.db = self.client[self.mongo_db]
+
+    
+
     def process_item(self, item, spider):
-        return item
+        bbcDict={}
+        if item['text'] != []:
+            bbcDict['headline']=item['headline']
+            bbcDict['author']=item['author']
+            bbcDict['text']=" ".join(item['text'])
+            bbcDict['url']=item['url']
+            self.db['articles'].insert_one(dict(bbcDict))
+            return item
+        
+    
+    def close_spider(self, spider):
+        self.client.close()
