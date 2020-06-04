20fb28a2e42bc5c67e75210a2896d5539bf3fed5
naimaelhafed <hafed.naima@gmail.com>
2020-06-05 00:36:37 +0100

    Version final

--- src/api_Theguardian/modproject.py
+++ src/api_Theguardian/modproject.py
@@ -14,18 +14,22 @@ from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.feature_extraction.text import TfidfTransformer
 from scipy.sparse import coo_matrix
 from collections import Counter
-#nltk.download('stopwords')
-#nltk.download('wordnet')
+nltk.download('stopwords')
+nltk.download('wordnet')
 
 # MongoDB Connection
 client = MongoClient('mongodb+srv://root:root@crawlertheguardian-sjvep.mongodb.net/crawlertheguardian?retryWrites=true&w=majority')
+#Name of Database
 db = client['crawlertheguardian']
 
 #recovery of articles
 def articles_of_mongodb():
+    # list of articles
     articles = []
+    #get of articles the collection  articles
     col = db.get_collection("articles")
     for document in col.find({}):
+        # add document in list of articles
         articles.append(document)
     #dataset=pd.DataFrame(articles)
     return articles
@@ -33,9 +37,13 @@ def articles_of_mongodb():
 
 #text Preprocessing
 def Preprocessing():
+    #recovery of articles
     dataset=articles_of_mongodb()
+    # Save articles in dataframe format
     dataset=pd.DataFrame(dataset)
+    # get a full list for english language
     stop_words = set(stopwords.words("English"))
+    # Delete of spaces between words
     text=dataset['text'].apply(lambda x: (str(x).split ("  ")))
     content=[]
     corpus = []
@@ -44,10 +52,12 @@ def Preprocessing():
     for i in range(0,len(text)):
         sen=[]
         for x in text[i]:
+        # Delete of space between caracters
             x=x.replace(" ","")
             sen.append(x)
 
         se=s.join(sen)
+        # Delete of comma between caracters
         se=se.replace(",","")
         content.append(se)
 
@@ -80,15 +90,19 @@ def Preprocessing():
 # recovery of the 10 keywords of a sentence
 def keywords(phrase):
     words = [word for word in phrase.split()]
+    # get the 10 keywords of a sentence
     wordcount = Counter(words).most_common(10)
     keywords=dict(wordcount)
     return list(keywords.keys())
 
 #Retrieving keywords from article text 
 def list_keyword():
+    #get of articles the collection  articles
     dataset=articles_of_mongodb()
+    #Save list of articles in dataframe format
     dataset=pd.DataFrame(dataset)
     liste=[]
+    # text Preprocessing 
     articles=Preprocessing()
     for i in range(0,len(articles)):
         liste.append(keywords(articles[i]))
@@ -96,8 +110,11 @@ def list_keyword():
 
 #Save articles in dataframe format   
 def dataframe_news():
+    #get of articles the collection  articles
     articles=articles_of_mongodb()
+    #Save list of articles in dataframe format
     dataset=pd.DataFrame(articles)
+    # text Preprocessing 
     text=Preprocessing()
     keyword_list=list_keyword()
     list_keywords=keyword_list
@@ -107,7 +124,9 @@ def dataframe_news():
 
 # Retrieving articles by keyword
 def getArticles(keyword):
+    #get of articles the collection  articles
     dataset=articles_of_mongodb()
+    #Save list of articles in dataframe format
     dataset=pd.DataFrame(dataset)
     dataset=dataframe_news()
     data=dataset[dataset['list_keywords'].apply(lambda x : any((i for i in x if i.find(keyword) >= 0 )))]
