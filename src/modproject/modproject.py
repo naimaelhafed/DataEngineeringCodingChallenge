feee48cca3bea98504af42bce060c8155e31a7d7
naimaelhafed <hafed.naima@gmail.com>
2020-06-04 17:00:17 +0100

    function Preprocessing()

--- src/modproject/modproject.py
+++ src/modproject/modproject.py
@@ -31,6 +31,53 @@ def articles_of_mongodb():
     return articles
 
 
+#text Preprocessing
+def Preprocessing():
+    dataset=articles_of_mongodb()
+    dataset=pd.DataFrame(dataset)
+    stop_words = set(stopwords.words("English"))
+    text=dataset['text'].apply(lambda x: (str(x).split ("  ")))
+    content=[]
+    corpus = []
+
+    s = ', '
+    for i in range(0,len(text)):
+        sen=[]
+        for x in text[i]:
+            x=x.replace(" ","")
+            sen.append(x)
+
+        se=s.join(sen)
+        se=se.replace(",","")
+        content.append(se)
+
+    keyword=[]
+    corpus = []
+    for i in range(0, len(content)):
+        #Remove punctuations
+        text = re.sub('[^a-zA-Z]', ' ', content[i])
+
+        #Convert to lowercase
+        text = text.lower()
+
+        #remove tags
+        text=re.sub("&lt;/?.*?&gt;"," &lt;&gt; ",text)
+
+        # remove special characters and digits
+        text=re.sub("(\\d|\\W)+"," ",text)
+
+        ##Convert to list from string
+        text = text.split()
+        ps=PorterStemmer()
+        #Lemmatisation
+        lem = WordNetLemmatizer()
+        text = [word for word in text if not word in stop_words] 
+        text = " ".join(text)
+
+        corpus.append(text)
+    return corpus
+
 
 if __name__ == "__main__":
-    print(articles_of_mongodb())
+    #print(articles_of_mongodb())
+    print(Preprocessing())
